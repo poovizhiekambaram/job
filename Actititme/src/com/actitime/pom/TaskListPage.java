3b2181064b5c0f46f092a9367301ebad4bb20013
epozh <epozh@LAPTOP-8SI5P0FC>
2024-04-16 12:46:52 +0530

    commit

---
+++ Actititme/src/com/actitime/pom/TaskListPage.java
@@ -0,0 +1,66 @@
+package com.actitime.pom;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class TaskListPage {
+@FindBy(xpath="//div[text()='Add New']")
+private WebElement addNewBtn;
+
+@FindBy(xpath="//div[text()='+ New Customer']")
+private WebElement newCustomerOption;
+	
+@FindBy(id="customerLightBox_nameField")
+private WebElement enterCustomerNameTbx;
+
+@FindBy(id="customerLightBox_descriptionField")
+private WebElement customerDescriptionTbx;
+
+@FindBy(xpath="//button[contains(text(),'Select')]")
+private WebElement selectDD;
+
+@FindBy(xpath="//a[text()='Our Company']")
+private WebElement ourCompanyOption;
+
+@FindBy(xpath="//span[text()='Create Customer']")
+private WebElement createCustomerBtn;
+
+@FindBy(xpath="//div[@class='navigationLinks']/../div[2]")
+private WebElement verifyCustomerName;
+
+public TaskListPage(WebDriver driver) {
+	PageFactory.initElements(driver, this);
+}
+public WebElement getAddNewBtn() {
+	return addNewBtn;
+}
+public WebElement getNewCustomerOption() {
+	return newCustomerOption;
+}
+public WebElement getEnterCustomerNameTbx() {
+	return enterCustomerNameTbx;
+}
+public WebElement getCustomerDescriptionTbx() {
+	return customerDescriptionTbx;
+}
+public WebElement getSelectDD() {
+	return selectDD;
+}
+public WebElement getOurCompanyOption() {
+	return ourCompanyOption;
+}
+public WebElement getCreateCustomerBtn() {
+	return createCustomerBtn;
+}
+public WebElement getVerifyCustomerName() {
+	return verifyCustomerName;
+}
+
+}
+
+
+
+
+
---
+++ Actititme/src/com/actitime/testscripts/CustomerModule.java
@@ -0,0 +1,43 @@
+package com.actitime.testscripts;
+
+import java.io.IOException;
+import java.time.Duration;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import com.actitime.generic.BaseClass;
+import com.actitime.generic.FileLib;
+import com.actitime.pom.HomePage;
+import com.actitime.pom.TaskListPage;
+
+@Listeners(com.actitime.generic.ListenerImplementation.class)
+public class CustomerModule extends BaseClass{
+
+	@Test
+public void testCreateCustomer() throws InterruptedException,EncryptedDocumentException, IOException  {
+	Reporter.log("CreateCustomer",true);
+	FileLib f=new FileLib();
+	String customerName = f.getExcelData("CreateCustomer", 1, 3);
+	String customerDescription = f.getExcelData("CreateCustomer", 1, 4);
+	HomePage h=new HomePage(driver);
+	h.setTaskTab();
+	TaskListPage t=new TaskListPage(driver);
+	t.getAddNewBtn().click();
+	t.getNewCustomerOption().click();
+	t.getEnterCustomerNameTbx().sendKeys(customerName);
+	t.getCustomerDescriptionTbx().sendKeys(customerDescription);
+	t.getSelectDD().click();
+	t.getOurCompanyOption().click();
+	t.getCreateCustomerBtn().click();
+WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+wait.until(ExpectedConditions.textToBePresentInElement(t.getVerifyCustomerName(),customerName));
+String actualCustomer = t.getVerifyCustomerName().getText();
+Assert.assertEquals(actualCustomer, customerName);
+	}	
+}
